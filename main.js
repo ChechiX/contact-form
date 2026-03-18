6b96dd77ccb16c13927de6fc6f5738d736a3dd3f
Julio Anccasi <julio.anccasi.dev@gmail.com>
2026-03-18 00:48:23 -0500

    Refactor form input names for consistency and update toast notification logic for improved user feedback

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+const toast = document.getElementById('toast');
 const contactForm = document.getElementById('contactForm');
 
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  const { firstName, lastName, email, queryType, message, consent } =
+    Object.fromEntries(new FormData(contactForm).entries());
+
+  console.log({ firstName, lastName, email, queryType, message, consent });
+
+  if (firstName && lastName && email && queryType && message && consent) {
+    toast.classList.add('toast--visible');
+
+    setTimeout(() => {
+      toast.classList.remove('toast--visible');
+    }, 3000);
+  }
 });
